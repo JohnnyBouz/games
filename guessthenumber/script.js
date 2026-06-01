2d9df549949d3b381bd8ccbea8fab274386da0f3
John Bouzoukas <giannis.bou@hotmail.com>
2026-06-02 01:56:20 +0300

    blahblah

--- guessthenumber/script.js
+++ guessthenumber/script.js
@@ -1,53 +1,55 @@
-let secretNumber;
-let attempts;
+let secretNumber = Math.floor(Math.random() * 100) + 1;
 
-function startGame() {
-    secretNumber = Math.floor(Math.random() * 100) + 1;
-    attempts = 0;
+let attempts = 0;
 
-    document.getElementById("attempts").textContent = attempts;
-    document.getElementById("message").textContent =
-        "Καλή επιτυχία!";
-    document.getElementById("guessInput").value = "";
-}
+function checkGuess(){
 
-function checkGuess() {
-    const guess = Number(document.getElementById("guessInput").value);
-    const message = document.getElementById("message");
+    let guess =
+        Number(document.getElementById("guessInput").value);
+
+    let message =
+        document.getElementById("message");
 
     attempts++;
-    document.getElementById("attempts").textContent = attempts;
 
-    if (!guess) {
-        message.textContent = "Please enter a number!";
-        message.style.color = "orange";
-        return;
-    }
+    document.getElementById("attempts").textContent =
+        attempts;
 
-    if (guess === secretNumber) {
-        message.textContent = `Congratulations! You found the number in ${attempts} attempts!`;
+    if(guess === secretNumber){
+
+        message.textContent =
+            "Congratulations! You found it!";
+
         message.style.color = "green";
-    } else if (guess < secretNumber) {
-        message.textContent = "Too low!";
+
+    }
+    else if(guess < secretNumber){
+
+        message.textContent =
+            "Too low!";
+
         message.style.color = "red";
-    } else {
-        message.textContent = "Too high!";
-        message.style.color = "purple";
+
+    }
+    else{
+
+        message.textContent =
+            "Too high!";
+
+        message.style.color = "red";
     }
 }
-if (guess === secretNumber)
-function newGame() {
-    secretNumber = Math.floor(Math.random() * 100) + 1;
+
+function newGame(){
+
+    secretNumber =
+        Math.floor(Math.random() * 100) + 1;
+
     attempts = 0;
 
     document.getElementById("attempts").textContent = 0;
-    document.getElementById("message").textContent = "";
-    document.getElementById("guessInput").value = "";
-}
 
-function toggleTheme() {
-    document.body.classList.toggle("dark");
-}
-function toggleTheme() {
-    document.body.classList.toggle("dark");
+    document.getElementById("message").textContent = "";
+
+    document.getElementById("guessInput").value = "";
 }
